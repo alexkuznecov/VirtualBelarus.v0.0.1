785bcca861b64f374ea0595036fc0ebe83e71500
alexkuznecov <kuzniecov@mail.ru>
2016-04-22 10:37:40 +0300

    Fixed user mapping

---
+++ VirtualBelarus.v0.0.1/src/main/java/by/grsu/service/ImageService.java
@@ -0,0 +1,19 @@
+package by.grsu.service;
+
+import by.grsu.domain.Image;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+public interface ImageService {
+
+    @Results(value = {
+            @Result(property = "id", column = "id"),
+            @Result(property = "url", column = "url"),
+            @Result(property = "path", column = "path")
+    })
+    @Select("select id,url,path from image where id = {#id}")
+    Image  getImageById(@Param(value = "id") Integer id);
+
+}
--- VirtualBelarus.v0.0.1/src/main/java/by/grsu/service/UserService.java
+++ VirtualBelarus.v0.0.1/src/main/java/by/grsu/service/UserService.java
@@ -1,18 +1,19 @@
 package by.grsu.service;
 
+import by.grsu.domain.Image;
 import by.grsu.domain.User;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 public interface UserService {
 
     @Results(value = {
+            @Result(property = "id", column = "id"),
             @Result(property = "name", column = "name"),
-            @Result(property = "telephone", column = "telephone"),
+            @Result(property = "country", column = "city"),
             @Result(property = "email", column = "email"),
-            @Result(property = "login", column = "login")
+            @Result(property = "login", column = "login"),
+            @Result(property = "avatar", column = "avatarId", javaType = Image.class,
+            one = @One(select = "by.grsu.service.ImageService.getImageById"))
     })
     @Select("select name,surname,telephone,email,login from users where login =#{login}")
     User getUserByLogin(@Param(value = "login") String login);
