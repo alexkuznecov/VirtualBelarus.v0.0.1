897f2489649906ae3748f2f9fac0fd52b93e56ae
alexkuznecov <kuzniecov@mail.ru>
2016-04-22 04:25:25 +0300

    Added location

---
+++ VirtualBelarus.v0.0.1/src/main/java/by/grsu/domain/Location.java
@@ -0,0 +1,34 @@
+package by.grsu.domain;
+
+public class Location {
+
+    private Integer id;
+
+    private Float latitude;
+
+    private Float longitude;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+}
