89f0485552ec4c9238f6f07607238fcc7bfacf73
alexkuznecov <kuzniecov@mail.ru>
2016-04-22 04:29:51 +0300

    Added hotSpot

---
+++ VirtualBelarus.v0.0.1/src/main/java/by/grsu/domain/HotSpot.java
@@ -0,0 +1,74 @@
+package by.grsu.domain;
+
+public class HotSpot {
+
+    private Integer id;
+
+    private String title;
+
+    private String about;
+
+    private Float height;
+
+    private Float width;
+
+    private Float x;
+
+    private Float y;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public Float getHeight() {
+        return height;
+    }
+
+    public void setHeight(Float height) {
+        this.height = height;
+    }
+
+    public Float getWidth() {
+        return width;
+    }
+
+    public void setWidth(Float width) {
+        this.width = width;
+    }
+
+    public Float getX() {
+        return x;
+    }
+
+    public void setX(Float x) {
+        this.x = x;
+    }
+
+    public Float getY() {
+        return y;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
+    }
+}
