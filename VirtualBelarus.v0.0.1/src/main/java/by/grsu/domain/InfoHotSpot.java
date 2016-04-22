12f0ce3d670c7054679d2a711dcdde47a73452fa
alexkuznecov <kuzniecov@mail.ru>
2016-04-22 04:41:59 +0300

    Added InfoHotSpot

---
+++ VirtualBelarus.v0.0.1/src/main/java/by/grsu/domain/InfoHotSpot.java
@@ -0,0 +1,34 @@
+package by.grsu.domain;
+
+public class InfoHotSpot {
+
+    private Integer id;
+
+    private Image image;
+
+    private HotSpot hotSpot;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public HotSpot getHotSpot() {
+        return hotSpot;
+    }
+
+    public void setHotSpot(HotSpot hotSpot) {
+        this.hotSpot = hotSpot;
+    }
+}
