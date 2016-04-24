e19078be370c7c122d56553d61b89865b97062b9
alexkuznecov <kuzniecov@mail.ru>
2016-04-24 19:29:53 +0300

    Added InfoHotSpotService

--- VirtualBelarus.v0.0.1/src/main/java/by/grsu/domain/TransitionHotSpot.java
+++ VirtualBelarus.v0.0.1/src/main/java/by/grsu/domain/TransitionHotSpot.java
@@ -5,4 +5,20 @@ public class TransitionHotSpot {
     private Integer id;
 
     private Scene scene;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
 }
--- VirtualBelarus.v0.0.1/src/main/java/by/grsu/service/InfoHotSpotSerivce.java
+++ VirtualBelarus.v0.0.1/src/main/java/by/grsu/service/InfoHotSpotSerivce.java
@@ -1,4 +1,22 @@
 package by.grsu.service;
 
+import by.grsu.domain.HotSpot;
+import by.grsu.domain.Image;
+import by.grsu.domain.InfoHotSpot;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
 public interface InfoHotSpotSerivce {
+
+    @Results(value = {
+            @Result(property = "id", column = "id", id=true),
+            @Result(property = "hotSpot", column = "hotSpotId", javaType = HotSpot.class,
+                    one = @One(select = "by.grsu.service.HotSpotService.getHotSpotById")),
+            @Result(property = "image", column = "imageId", javaType = Image.class,
+                    one = @One(select = "by.grsu.service.ImageService.getImageById"))
+    })
+    @Select("select * from infoHotSpot where scenaId = #{scenaId}")
+    List<InfoHotSpot> getInfoHotSpotsByScenaId(@Param(value = "scenaId") Integer scenaId);
+
 }
