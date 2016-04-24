e2f9a518cb205bc861c84777ec71f65a3e28ff75
alexkuznecov <kuzniecov@mail.ru>
2016-04-24 19:00:43 +0300

    Added locaitonService

--- VirtualBelarus.v0.0.1/src/main/java/by/grsu/service/LocationService.java
+++ VirtualBelarus.v0.0.1/src/main/java/by/grsu/service/LocationService.java
@@ -1,4 +1,19 @@
 package by.grsu.service;
 
+import by.grsu.domain.Location;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
 public interface LocationService {
+
+    @Results(value = {
+            @Result(property = "id", column = "id", id = true),
+            @Result(property = "latitude", column = "latitude"),
+            @Result(property = "longitude", column = "longitude")
+    })
+    @Select("select * from location where id = #{id}")
+    Location getLocationById(@Param(value = "id") Integer id);
+
 }
